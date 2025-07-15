ec963e4fd7243caa64a4d7dd450711a98c3c64e3
mohammad iko <developer.iko.mike@gmail.com>
2025-07-15 05:54:21 -0700

    Header responsive

--- F/src/Components/header/navBar/NavBar.jsx
+++ F/src/Components/header/navBar/NavBar.jsx
@@ -5,11 +5,13 @@ import Search from "./SVGs/Search";
 import Basket from "./SVGs/Basket";
 import AuthContext from "../../../Components/context/authContext";
 import { Link } from "react-router-dom";
+import Accordion from "react-bootstrap/Accordion";
 
 export default function NavBar() {
   let authContext = useContext(AuthContext);
 
   const [allMenus, setAllMenus] = useState([]);
+  const [isShowBurgerMenu, setIsShowBurgerMenu] = useState(false);
 
   useEffect(() => {
     fetch("http://localhost:4000/v1/menus")
@@ -21,6 +23,87 @@ export default function NavBar() {
     <div className="main-header">
       <div className="container-fluid">
         <div className="main-header__content">
+          <div
+            className="main-header__midd w5 h5 bsdl br10 dnone jcc aic cp"
+            onClick={() => setIsShowBurgerMenu(true)}
+          >
+            <span className="main-header__midd__burgerMenu"></span>
+            <div>
+              <ul
+                className="main-header__menu____mobile dnone ps"
+                style={{ right: isShowBurgerMenu ? 0 : -400 }}
+              >
+                <li
+                  className="main-header__menu____mobile_____logo tc"
+                  style={{ height: "6rem" }}
+                >
+                  <img
+                    src="/logo/Logo.png"
+                    className="main-header__logo"
+                    alt="لوگوی سبزلرن"
+                  />
+                </li>
+                <li className="main-header__item main-header__item____mobile fs1-5">
+                  <Link to="/" className="main-header__link">
+                    صفحه اصلی
+                  </Link>
+                </li>
+
+                {allMenus.length &&
+                  allMenus.map((menu) =>
+                    menu.submenus.length ? (
+                      <Accordion defaultActiveKey="1">
+                        <Accordion.Item eventKey="0" className="accordion">
+                          <Accordion.Header className="main-header__menu____mobile____burger______headerAccordion">
+                            {menu.title}
+                          </Accordion.Header>
+                          {menu.submenus.map((subMenu) => (
+                            <Accordion.Body className="accordion-body introduction__accordion-body">
+                              <Link
+                                to={subMenu.href}
+                                className="main-header__link"
+                              >
+                                {subMenu.title}
+                              </Link>
+                            </Accordion.Body>
+                          ))}
+                        </Accordion.Item>
+                      </Accordion>
+                    ) : (
+                      <li className="main-header__item main-header__item____mobile fs1-5">
+                        <Link to="/" className="main-header__link">
+                          {menu.title}
+                        </Link>
+                      </li>
+                    )
+                  )}
+                <li className="df w100">
+                  <Link to="/" className="main-header__search-btn fg">
+                    <Search />
+                  </Link>
+                  <Link to="/" className="main-header__cart-btn fg">
+                    <Basket />
+                  </Link>
+                </li>
+                <li>
+                  {authContext.userInfos.name ? (
+                    <Link to="/" className="main-header__profile">
+                      <span className="main-header__profile-text">
+                        {authContext.userInfos.name}
+                      </span>
+                    </Link>
+                  ) : (
+                    <Link to="/register" className="main-header__profile">
+                      <span className="main-header__profile-text">
+                        ورود / ثبت نام
+                      </span>
+                    </Link>
+                  )}
+                </li>
+              </ul>
+            </div>
+          </div>
+
           <div className="main-header__right">
             <img
               src="/logo/Logo.png"
--- F/src/Components/header/topBar/TopBar.jsx
+++ F/src/Components/header/topBar/TopBar.jsx
@@ -4,38 +4,66 @@ import MailSVG from "./SVGs/MailSVG";
 import PhoneSVG from "./SVGs/PhoneSVG";
 import { Link } from "react-router-dom";
 
-export default memo( function TopBar() {
-
-  const [allItemTopBar , setAllItemTopBar] = useState([])
+export default memo(function TopBar() {
+  const [allItemTopBar, setAllItemTopBar] = useState([]);
+  const [countItem, setCountItem] = useState(3);
 
   useEffect(() => {
     fetch("http://localhost:4000/v1/menus/topbar")
-      .then(res => res.json())
-          .then(data => setAllItemTopBar(data));
-  }, [])
+      .then((res) => res.json())
+      .then((data) => setAllItemTopBar(data));
+  }, []);
 
   // useEffect(() => { getRandomItemFromTopBarItem(allItemTopBar,5) } , [allItemTopBar])
 
-  const getRandomItemFromTopBarItem = (arr , countItem) => {
-    const itemStatic = [...arr].sort(() => 0.5 - Math.random() * 5)
-    return itemStatic.slice(0 , countItem)
-  }
-  
+  const getRandomItemFromTopBarItem = (arr, countItem) => {
+    const itemStatic = [...arr].sort(() => 0.5 - Math.random() * 5);
+    return itemStatic.slice(0, countItem);
+  };
+
+  useEffect(() => {
+    const width = window.innerWidth;
+
+    switch (true) {
+      case width < 576:
+        setCountItem(0)
+        break;
+      case width >= 576 && width < 768:
+        setCountItem(1)
+        break;
+      case width >= 768 && width < 992:
+        setCountItem(2)
+        break;
+      case width >= 992 && width < 1200:
+        setCountItem(3)
+        break;
+      case width >= 1200 && width < 1400:
+        setCountItem(4)
+        break;
+      case width >= 1400:
+        setCountItem(5)
+        break;
+      default:
+        setCountItem(3)
+    }
+  }, []);
+
   return (
     <div className="top-bar">
       <div className="container-fluid">
         <div className="top-bar__content">
           <div className="top-bar__right">
             <ul className="top-bar__menu">
-              {
-                allItemTopBar && getRandomItemFromTopBarItem(allItemTopBar , 5).map(topbarItem => (                  
-              <li className="top-bar__item" key={topbarItem._id}>
-                <Link to={topbarItem.href} className="top-bar__link">
-                  {topbarItem.title}
-                </Link>
-              </li>
-                ))
-              }
+              {allItemTopBar &&
+                getRandomItemFromTopBarItem(allItemTopBar, countItem).map(
+                  (topbarItem) => (
+                    <li className="top-bar__item" key={topbarItem._id}>
+                      <Link to={topbarItem.href} className="top-bar__link">
+                        {topbarItem.title}
+                      </Link>
+                    </li>
+                  )
+                )}
             </ul>
           </div>
           <div className="top-bar__left">
@@ -58,5 +86,4 @@ export default memo( function TopBar() {
       </div>
     </div>
   );
-}
-)
+});
